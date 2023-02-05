55b39345de94d2658201f3bcd1d5e9069abe7e83
dxenonb <drdxbme@gmail.com>
2023-02-05 15:40:35 -0500

    Add more output to server test

--- tests/server.rs
+++ tests/server.rs
@@ -1,9 +1,12 @@
 use rust_game_networking::{
-    config::ClientServerConfig, initialize, server::Server, shutdown, PRIVATE_KEY_BYTES,
+    config::ClientServerConfig, initialize, log_level, server::Server, shutdown, LogLevel,
+    PRIVATE_KEY_BYTES,
 };
 
-fn main() {
+#[test]
+fn server() {
     initialize().unwrap();
+    log_level(LogLevel::Debug);
 
     server_main();
 
@@ -17,10 +20,16 @@ fn server_main() {
     let max_clients = 16;
     let private_key = [0; PRIVATE_KEY_BYTES];
 
-    let mut server = Server::new(&private_key, "127.0.0.1:40000".to_string(), config, time);
+    let server_address = "127.0.0.1:40000".to_string();
+    println!("starting server on port {} (insecure)", &server_address);
+
+    let mut server = Server::new(&private_key, server_address, config, time);
     server.start(max_clients);
+
+    println!("server started");
 
     // TODO: loop
 
+    println!("stopping server");
     server.stop();
 }
