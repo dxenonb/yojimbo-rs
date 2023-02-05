f6a553eb0aea9a076625a92a46fde7f7ef7e845d
dxenonb <drdxbme@gmail.com>
2023-02-05 02:15:09 -0500

    Commit helper macro (prev commits don't build)

--- src/bindings.rs
+++ src/bindings.rs
@@ -2,4 +2,15 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[macro_export]
+macro_rules! gf_init_default {
+    ($strukt:ty, $default_function:ident) => {
+        unsafe {
+            let mut obj: MaybeUninit<$strukt> = MaybeUninit::uninit();
+            $default_function(obj.as_mut_ptr());
+            obj.assume_init()
+        }
+    };
+}
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
