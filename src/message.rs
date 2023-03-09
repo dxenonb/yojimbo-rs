24d92db5e6726ce8d4cae86e16a1487f2249c6d6
dxenonb <drdxbme@gmail.com>
2023-03-09 16:30:56 -0500

    Require 'static for NetworkMessage (probably permanent)

--- src/message.rs
+++ src/message.rs
@@ -8,7 +8,7 @@ use std::{
 /// NOTE: Clone should be a temporary requirement. This is a stop-gap solution
 /// that simplifies porting reliable channels; I have a design in mind that
 /// should eliminate the Clone requirement but want to get it working first.
-pub trait NetworkMessage: Clone
+pub trait NetworkMessage: Clone + 'static
 where
     Self: Sized,
 {
